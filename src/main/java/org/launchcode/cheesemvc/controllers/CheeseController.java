e74c2d4e88be46c33ea9d75615d6bfafa396781c
kamakshiSK <kamakshi.raibagkar@gmail.com>
2019-05-23 11:51:16 -0500

    Thymeleaf condition check and for loop

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -5,13 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping(value="cheese")
 public class CheeseController {
 
     @RequestMapping(value="")
     public String index(Model model){
+
+        ArrayList<String> chesses = new ArrayList<>();
+        chesses.add("Parmesan");
+        chesses.add("Cheddar");
+        chesses.add("Sharp Cheddar");
+        chesses.add("Colby Jack");
+
         model.addAttribute("title", "My Cheese ");
+        model.addAttribute("cheeses", chesses );
+
         return "cheese/index";
     }
 }
