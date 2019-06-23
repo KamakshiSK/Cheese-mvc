337928564c9a7a2f870c0b8680f288bdfd2ecbe1
kamakshiSK <kamakshi.raibagkar@gmail.com>
2019-06-23 02:05:07 -0500

    MenuController - View Menu forms

--- src/main/java/org/launchcode/cheesemvc/controllers/MenuController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/MenuController.java
@@ -1,10 +1,18 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Menu;
 import org.launchcode.cheesemvc.models.data.CheeseDao;
 import org.launchcode.cheesemvc.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "menu")
@@ -17,4 +25,33 @@ public class MenuController {
     private MenuDao menuDao;
 
 
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String addMenuForm(Model model) {
+        model.addAttribute("title", "MyCheeses: Add Menu");
+        model.addAttribute(new Menu());
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddMenuForm(@ModelAttribute @Valid Menu menu, Model model, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "MyCheeses: Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId();
+    }
+
+    @RequestMapping(value = "view/{menuId}", method = RequestMethod.GET)
+    public String viewMenuForm(Model model, @PathVariable int menuId) {
+        Menu menu = menuDao.findOne(menuId);
+        model.addAttribute(menu);
+
+        model.addAttribute("title", "My Cheeses: ");
+
+        return "menu/view";
+
+    }
 }
